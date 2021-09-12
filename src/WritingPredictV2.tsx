bf766f50384f2ed321250633aa24e82039c10410
Ishan Dongol <ishandongol@gmail.com>
2021-09-12 12:22:17 +0545

    Loading state

--- src/WritingPredictV2.tsx
+++ src/WritingPredictV2.tsx
@@ -18,38 +18,39 @@ interface PredictionState extends PredictionProps{
 export const WritingPredictV2 = () => {
   const [{ canvas, isReady, ...state }, { init, clear,handleClear, ...api }] = useCanvas();
   const [prediction,setPrediction] = useState<PredictionState>(initialState)
+  const [loading,setLoading] = useState<boolean>(false)
   const clearPrediction = () => {
     handleClear()
     setPrediction(initialState)
   }
   const handlePredict = useCallback(() => {
     if (!canvas || !canvas.current) return;
+    setLoading(true)
     let blob = '';
     const xhr = new XMLHttpRequest();
     xhr.open("GET", canvas.current.toDataURL("image/jpg;base64;", 0.5));
     xhr.responseType = "blob";// force the HTTP response, response-type header to be blob
-    xhr.onload = () => {
-
+    xhr.onload = async () => {
       blob = xhr.response;// xhr.response is now a blob object
       console.log(blob)
       const formData = new FormData();
       formData.append("image", blob);
-      axios.post(BASE_URL + "/upload", formData).then(response => {
-        if (response) {
-          axios.get(BASE_URL + "/predict").then(resp =>
-            setPrediction({
-              prediction: resp.data.prediction,
-              probability: resp.data.probability,
-              showPrediction: true,
-            })
-          ).catch(() => {
-            toast.warn("Failed to predict")
-        });
-
-        }
-      }).catch(() => {
-          toast.warn("Failed to upload image")
-      });
+      try{
+        const response = await axios.post(BASE_URL + "/upload", formData)
+      if (response) {
+        const resp = await axios.get(BASE_URL + "/predict");
+        setPrediction({
+          prediction: resp.data.prediction,
+          probability: resp.data.probability,
+          showPrediction: true,
+        })
+      }
+      }catch(err){
+        toast.warn("Failed to Predict, Server Error")
+      } finally {
+        setLoading(false)
+      }
+      
     }
     xhr.send();
 
@@ -80,6 +81,7 @@ export const WritingPredictV2 = () => {
      </div>
       <Toolbar
         {...toolbarProps}
+        loading={loading}
         className="p-4 self-center"
       />
     </div>
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,32 +1,35 @@
 import React from "react";
 
 export const Toolbar: React.FC<any> = ({
-    handlePredict,
-    handleClear,
-    className,
+  handlePredict,
+  handleClear,
+  className,
+  loading,
 }) => {
-    return (
-        <div className={`${className}`} style={{width:'500px'}}>
-            <div className="py-3 flex">
-            <a href={"/"} className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 text-center my-4 rounded-full md:block mr-4 md:ml-0 flex-grow" onClick={(e) => {
-                    e.preventDefault()
-                    handleClear()
-                }}>
-                    Clear
-          </a>
-                <a
-                    className="bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-4 text-center my-4  ml-4 rounded-full md:block flex-grow"
-                    onClick={(e) =>{
-                        e.preventDefault()
-                        handlePredict()
-                    }}
-
-                    href={"/"}
-                >
-                    Predict
-          </a>
-                
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${className}`} style={{ width: "500px" }}>
+      <div className="py-3 flex">
+        <button
+          className={`bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 text-center my-4 rounded-full md:block mr-4 md:ml-0 flex-grow ${loading ? 'disabled:opacity-50' :''}`}
+          onClick={(e) => {
+            e.preventDefault();
+            handleClear();
+          }}
+          disabled={loading}
+        >
+          Clear
+        </button>
+        <button
+          className={`bg-purple-700 hover:bg-purple-900 text-white font-bold py-2 px-4 text-center my-4  ml-4 rounded-full md:block flex-grow ${loading ? 'disabled:opacity-50' :''}`}
+          onClick={(e) => {
+            e.preventDefault();
+            handlePredict();
+          }}
+          disabled={loading}
+        >
+          {loading ? "Please wait..." : "Predict"}
+        </button>
+      </div>
+    </div>
+  );
 };
